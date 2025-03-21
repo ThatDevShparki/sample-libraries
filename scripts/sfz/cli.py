1cf0b70aad6195ab87f164156c0bc6d93e1663fe
ThatDevShparki <shparki@thatdev.io>
2025-03-21 18:51:46 -0500

    feat: better looping

--- scripts/sfz/cli.py
+++ scripts/sfz/cli.py
@@ -132,6 +132,8 @@ def generate(path: str) -> None:
                 'ampeg_release': 0.45,
                 'amp_veltrack': 0,
                 'loop_mode': 'loop_sustain',
+                'loop_start': 300,
+                'loop_end': 700,
             },
         )
 
